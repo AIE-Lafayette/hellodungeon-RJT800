c53dae8e32b2ef6e7131135ef6b5b6129f83833a
Reginald Thompson <reginaldjthompsonjr23@gmail.com>
2023-09-01 12:00:23 -0500

    implemented dialogue and begun fleshing out combat.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -13,6 +13,64 @@ namespace HelloDungeon
 {
     class Game
     {
+
+
+        //mock menu function
+        //string TBD_Menu(string pathway, string playerChoice)
+        //{
+        //    string playerChoice = "0";
+
+        //    while (playerChoice != "1" && playerChoice != "2" && playerChoice != "3")
+        //    {
+        //        //display promblems and solutions
+        //        Console.Clear();
+        //        Console.WriteLine(prompt);
+
+        //        Console.WriteLine("1." + option1);
+
+        //        Console.WriteLine("2." + option2);
+
+        //        Console.WriteLine("3" + option3);
+
+        //        if (option3 != "")
+        //        {
+        //            Console.WriteLine("3." + option3);
+        //        }
+
+        //        //player input
+        //        Console.WriteLine("> ");
+        //        playerChoice = Console.ReadLine();
+
+        //        // if invalid input,
+        //        if (playerChoice != "1" && playerChoice != "2")
+        //        {
+        //            if (playerChoice == "3"&& option3 != "")
+        //            {
+        //                continue;
+        //            }
+
+        //            //
+        //            Console.Clear();
+        //            Console.WriteLine("Invalid Input");
+        //            Console.WriteLine("press a button");
+        //            Console.ReadKey(true);
+
+        //        }
+
+        //    }
+
+        //    return playerChoice;
+        //}
+
+        // mock combat sequence
+        // turn based battle between player and opposing forces
+        // gives players two actions per turn to attack, heal, or apply a buff
+        // on enemy turn, each enemy gets a to preform a action such as attack, defend, buff, or debuff
+        // allows for more enemies and even duplicate enemies as well
+
+        void
+
+
         //the mock damage calculator function
         float CalculatePain(float health, float attack, float defense)
         {
@@ -21,42 +79,36 @@ namespace HelloDungeon
             return totalDamage;
         }
 
-        //mock menu function
-        string TBD_Menu(string pathway, string playerChoice)
+        // mock beggining dialogue sequence
+
+        void BeginDialogue(string Text)
         {
-            string playerChoice = "0";
+            Console.WriteLine(Text);
+            Console.WriteLine("PRESS A BUTTON TO ADVANCE DIALOGUE, IT IS RECOMMNENDED TO PLAY WITH CAPS LOCK ON");
+            Console.ReadKey(true);
+            
+        }
 
-            while (playerChoice !="1"&& playerChoice !="2"&&playerChoice !="3")
-            {
-                //display promblems and solutions
-                Console.Clear();
-                Console.WriteLine(prompt);
+        // mock dialogue sequence?
+        void Dialogue(string Text)
+        {
+            Console.WriteLine(Text);
+            Console.ReadKey(true);
+            
+        }
 
-                Console.WriteLine("1." + option1);
 
-                Console.WriteLine("2." + option2);
+        // mock dialouge advancing function
+        void EndDialogue(string Text)
+        {
+            Console.WriteLine(Text);
+            Console.WriteLine("" +
+                            "" +
+                            "");
+            Console.ReadKey(true);
 
-                Console.WriteLine("3" + option3);
 
-               
-                //player input
-                Console.WriteLine("> ");
-                playerChoice = Console.ReadLine();
 
-                // if invalid input,
-                if (playerChoice != "1" && playerChoice != "2" && playerChoice != "3")
-                {
-                    //
-                    Console.Clear();
-                    Console.WriteLine("Invalid Input");
-                    Console.WriteLine("press a button");
-                    Console.ReadKey(true);
-
-                }
-
-            }
-
-            return playerChoice;
         }
 
 
@@ -70,6 +122,15 @@ namespace HelloDungeon
             return;
         }
 
+        //mock adding function
+        int add(int a,int b)
+        {
+            int result;
+
+           result = a + b;
+            Console.WriteLine(result);
+            return result; 
+        }
 
         //float Alligator (float a,float b)
         //{
@@ -114,14 +175,11 @@ namespace HelloDungeon
                 
         //    }
 
-
-
-
         //}
 
         public void Run()
         {
-            Console.WriteLine();
+            //add(5,-6);
             //Console.WriteLine(Alligator(40f, 42f));
             //Console.ReadKey(true);
             //Console.Clear();
@@ -136,7 +194,7 @@ namespace HelloDungeon
 
             
 
-            return;
+            
             //states the health, defense, and speed stats for J-CCC (Protagonist)
             string playerName = "J-CCC";
             string playerChoice = "";
@@ -147,42 +205,63 @@ namespace HelloDungeon
 
             //states all moves and options for J-CCC
 
-            
-                bool BasicStrike = true;
-                float BasicStrikeDamage = 50f;
-
-                bool SelfDestruct = true;
-                float SelfDestructDamage = 999999999f;
-
-                bool HasFlamethrower = false;
-                float FlamethrowerDamage = 80f;
-
-                bool HasRailgun = false;
-                float RailgunDamage = 240f;
-
+                //gatling gun weapon
                 bool HasGatlingGun = false;
                 float GatlingGunDamage = 500f;
+            1         
 
-                bool HasStasisBubble = false;
-                float StasisBubbleDamage = 0f;
+                //flamethrower weapon
+                bool HasFlamethrower = false;
+                float FlamethrowerDamage = 80f;
+            2
 
+                // railgun weapon
+                bool HasRailgun = false;
+                float RailgunDamage = 240f;
+            3
+
+                //freeze ray weapon
+                bool HasFreezeBlaster =false;
+                float FreezerBlaster = 150f;
+            4
+                
+                //laser gun weapon
                 bool HasIonLaser = false;
                 float IonLaserDamage = 1000f;
+            5
 
-            
+                //stasisbubble tool
+                bool HasStasisBubble = false;
+                float StasisBubbleDamage = 0f;
+            6
 
+                // rocketbooster buff
+                bool HasRocketJets = false;
+                float RocketJetsDamgage = 0f;
+            7
+
+                // healing
+                bool SelfRepair = true;
+                float SelfRepairHealing = -1500f;
+            8
+
+
+                // kamikaze attack
+                bool SelfDestruct = true;
+                float SelfDestructDamage = 999999999f;
+            /0
 
             //states all enemies stats and misc.
             
 
             //states the stats of the stalker
-            string stalkerAbout = "A slightly below-average sized, skinny creature with white, almost luminescent skin. They appear to be incrediblymalnorished, with build being more bone than flesh.\n Their hunting tactics revolves around observing their prey to get a read on their movement and searching for weak points to attack with lighting fast claw strikes when the target is most vulnerable. Their first priority is conserving energy, which explains their reserved mannered of fighting.";
+            string stalkerAbout = "A slightly below-average sized, skinny creature with white, almost luminescent skin. They appear to be incredibly malnorished, with build being more bone than flesh.\n Their hunting tactics revolves around observing their prey to get a read on their movement and searching for weak points to attack with lighting fast claw strikes when the target is most vulnerable. Their first priority is conserving energy, which explains their reserved mannered of fighting.";
             float stalkerHealth = 90f;
             float stalkerDefense = .90f;
             float stalkerSpeed = 60f;
             bool stalkerDead = stalkerHealth <= 0;
 
-            Console.WriteLine(stalkerAbout);
+            //Console.WriteLine(stalkerAbout);
 
             //states the stats of the sticker
             string stickerAbout = "";
@@ -191,26 +270,31 @@ namespace HelloDungeon
             //states the stats of the lackey
             float lackeyHealth = 25;
 
-            
 
 
-            
+
+
 
             //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
             //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
             //this is at the start of the game that establishs the story and objective (INTRODUCTION)
 
-            Console.WriteLine("TWIST OF FATE");
-            Console.WriteLine("PRESS A BUTTON TO ADVANCE DIALOGUE, IT IS RECOMMNENDED TO PLAY WITH CAPS LOCK ON");
-            Console.ReadKey(true);
-            Console.WriteLine("You were once a lowly Janus bot, built and tasked to keep the ship INDAGATUS II clean and sterile.");
-            Console.ReadKey(true);
-            Console.WriteLine("However, things took a turn for the worse when the ship made impact with a large asteroid, infested with monsters.");
-            Console.ReadKey(true);
-            Console.WriteLine("With an monster invasion underway and all security Antesignano wiped out, it is up to you save the ship's inhabitants from annilation!");
-            Console.ReadKey(true);
-            Console.WriteLine("Can you change your fate? Y/N");
+            //Advance();
+
+            BeginDialogue("TWIST OF FATE");
+            Dialogue("TIME: AUGUST 17TH, 3492");
+            Dialogue("LOCATION: SECTOR X-79");
+            Dialogue("VESSEL NAME: INDAGATUS II");
+            Dialogue()
+
+            Dialogue("You were once a lowly Janus bot, built and tasked to keep the ship INDAGATUS II clean and sterile.");
+            Dialogue("However, things took a turn for the worse when the ship made impact with a large asteroid, infested with monsters.");
+            Dialogue("With an monster invasion underway and all security Antesignano wiped out, it is up to you save the ship's inhabitants from annilation!");
+            EndDialogue("Can you change your fate? Y/N");
+
+
+            
 
             while (playerChoice!="Y" && playerChoice!="N")
             
@@ -368,6 +452,7 @@ namespace HelloDungeon
                 Console.WriteLine("THE SHIP FALLS TO CHAOS, CLAIMED BY THE CREATURES");
                 Console.ReadKey(true);
                 Console.WriteLine("THERE WERE NO SURVIVORS");
+                return;
             }
             else
             {
